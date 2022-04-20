7916a357c079387f9381f8550a2c8e40e2e74548
Michael <michaelf6988@gmail.com>
2022-04-20 15:31:31 +0100

    removed variables

--- src/Cat.js
+++ src/Cat.js
@@ -1,60 +1,51 @@
-import { useEffect, useState } from 'react'
-import './App.css';
-import { faker } from '@faker-js/faker';
-import Basket from './Basket';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { faker } from "@faker-js/faker";
+import Basket from "./Basket";
 
 function Cat() {
   const [cats, setCats] = useState([]);
   // const [basket, setBasket] = useState([]);
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
-// Michaels_Branch
-  const petGender = faker.name.firstName()
-  const petSpecies = faker.animal.cat()
-  const petPrice = faker.finance.amount(700, 1000, 2, '$')
-  
+  //   const addBasket = (item) => {
+  //     setBasket([...basket, item])
+  // }
 
-//   const addBasket = (item) => {
-//     setBasket([...basket, item])
-// }
-
-
-// >>>>>>> main
+  // >>>>>>> main
   useEffect(() => {
     const fetchCats = async () => {
       try {
-        
-        const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=20')
-        if (!response.ok){
-          throw new Error(response.statusText)
+        const response = await fetch(
+          "https://api.thecatapi.com/v1/images/search?limit=20"
+        );
+        if (!response.ok) {
+          throw new Error(response.statusText);
         }
         const data = await response.json();
         setCats(data);
-        
       } catch (err) {
-        console.log(err.message)
-        setError("Could not fetch error")
-}}
-fetchCats();
+        console.log(err.message);
+        setError("Could not fetch error");
+      }
+    };
+    fetchCats();
   }, []);
-
-
 
   return (
     <div className="Cat">
-      {cats.map( (cat, index) => (
+      {cats.map((cat, index) => (
         <>
-        {error && <p>{error}</p>}
-        <h1>{faker.name.firstName()}</h1>
-        <h1>{faker.animal.cat()}</h1>
-        <h1>{faker.finance.amount(700, 1000, 2, '£')}</h1>
-        <img src={cat.url} alt="Cat"/>
-        <Basket key={index} />
+          {error && <p>{error}</p>}
+          <h1>{faker.name.firstName()}</h1>
+          <h1>{faker.animal.cat()}</h1>
+          <h1>{faker.finance.amount(700, 1000, 2, "£")}</h1>
+          <img src={cat.url} alt="Cat" />
+          <Basket key={index} />
         </>
       ))}
     </div>
   );
-
 }
 
 export default Cat;
