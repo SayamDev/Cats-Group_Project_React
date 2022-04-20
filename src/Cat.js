f2e31d051cbda84a29c11e403ab669f860eec5d4
Elliot Juniper <elliotjuniper@gmail.com>
2022-04-20 10:54:25 +0100

    cats

---
+++ src/Cat.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import './App.css';
+import { faker } from '@faker-js/faker';
+
+function Cat() {
+  const [cats, setCats] = useState([]);
+  const [error, setError] = useState(null)
+
+  const petName = faker.animal.cat()
+  useEffect(() => {
+  const fetchCats = async () => {
+    try {
+   
+    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=20')
+    if (!response.ok){
+      throw new Error(response.statusText)
+    }
+    const data = await response.json();
+    setCats(data);
+  
+} catch (err) {
+  console.log(err.message)
+  setError("Could not fetch error")
+}}
+fetchCats();
+  }, []);
+
+
+
+  return (
+    <div className="Cat">   
+      {cats.map( (cat) => (
+        <>
+        {error && <p>{error}</p>}
+        <h1>{petName}</h1>
+        <img src={cat.url} alt="Cat"/>
+        </>
+      ))}
+    </div>
+  );
+
+}
+
+export default Cat;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Cat from './Cat';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Cat />);
+
