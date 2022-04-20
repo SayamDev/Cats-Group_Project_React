8ef4fa114414b2d3d1ed7b033a074d2ff41facba
Elliot Juniper <elliotjuniper@gmail.com>
2022-04-20 14:25:35 +0100

    7

--- src/Cat.js
+++ src/Cat.js
@@ -1,22 +1,24 @@
 import { useEffect, useState } from 'react'
 import './App.css';
 import { faker } from '@faker-js/faker';
+import Basket from './Basket';
 
 function Cat() {
   const [cats, setCats] = useState([]);
-  const [basket, setBasket] = useState([]);
+  // const [basket, setBasket] = useState([]);
   const [error, setError] = useState(null)
 
 // Michaels_Branch
-
-
   const petGender = faker.name.firstName()
   const petSpecies = faker.animal.cat()
   const petPrice = faker.finance.amount(700, 1000, 2, '$')
   
-  const addBasket = (item) => {
-    setBasket([...basket, item])
-}
+
+//   const addBasket = (item) => {
+//     setBasket([...basket, item])
+// }
+
+
 // >>>>>>> main
   useEffect(() => {
     const fetchCats = async () => {
@@ -40,16 +42,14 @@ fetchCats();
 
   return (
     <div className="Cat">
-      <button
-          basket={basket}>Add to Basket
-        </button>   
-      {cats.map( (cat) => (
+      {cats.map( (cat, index) => (
         <>
         {error && <p>{error}</p>}
         <h1>{faker.name.firstName()}</h1>
         <h1>{faker.animal.cat()}</h1>
         <h1>{faker.finance.amount(700, 1000, 2, '£')}</h1>
         <img src={cat.url} alt="Cat"/>
+        <Basket key={index} />
         </>
       ))}
     </div>
