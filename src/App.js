e12def4ec60b492dbf8e18a18f5eb2db71ee2e4f
Elliot Juniper <elliotjuniper@gmail.com>
2022-04-21 16:10:04 +0100

    18

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import { useEffect, useState } from 'react'
 import './App.css';
-// import Basket from './Basket';
 import Navbar from "./Navbar"
 import Data from './Data'
 import React from "react";
 import Modal from './Modal'
-// import { Name } from '@faker-js/faker/name';
-// import Routing from "./Routing"
-// import { Container } from './App.styled';
 
 
 
@@ -15,31 +11,8 @@ import Modal from './Modal'
 
 const App = () => {
 
-  // const [cats, setCats] = useState([]);
-  // const [error, setError] = useState(null)
-
   const [basket, setBasket] = useState([]);
   const [show, setShow] = useState(false)
-  // const [itemCount, setItemCount] = React.useState(1);
-
-  // const {cat} = Data;
-  
-
-  // const handleAddProduct = (product) => {
-  //   const ProductExist = basket.find((cat) => cat.name === product.name);
-  //   if (ProductExist) {
-  //     setBasket(
-  //       basket.map((cat) =>
-  //       cat.name === product.name
-  //       ? {...ProductExist, quantity: ProductExist.quantity + 1}
-  //       :cat
-  //     )
-  //     )
-  //   }
-  //   else {
-  //     setBasket([...basket, {...product, quantity: 1}])
-  //   }
-  // }
 
     const handleAddProduct = (i) => {
       setBasket([...basket, i])
--- src/Data.js
+++ src/Data.js
@@ -77,33 +77,4 @@ const Data = ({handleAddProduct}) => {
   );
 }
 
-    {/* return (
-     <div className="Cat">  
-     <div className='wrapper'>
-          {cats.map( (cat,  
-           handleAddProduct
-         ) => (
-          <>
-          {error && <p>{error}</p>}
-          <div className='cat-container'> 
-          <h1>{faker.name.firstName()}</h1>
-          <h1>{faker.animal.cat()}</h1>
-          <img src={cat.url} alt="Cat"/>
-          <div className='addTo'>
-          <h1>£{faker.finance.amount(700, 1000, 2, )}</h1>
-          <button className="addBasket" 
-          // onClick={handleAddProduct(cat)}
-          >Add to Basket</button>
-          <div>
-          </div>
-        </div>
-        </div>
-          </>
-          ))}
-        </div>
-        )
-      </div>
-      
-    );
-}   */}
 export default Data
